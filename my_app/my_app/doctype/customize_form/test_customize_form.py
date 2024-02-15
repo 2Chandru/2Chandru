c93f173577761f73345029f1b3567687a4468953
2Chandru <chandru231100@gmail.com>
2024-02-15 10:55:04 +0530

    commit

--- my_app/my_app/doctype/customize_form/test_customize_form.py
+++ my_app/my_app/doctype/customize_form/test_customize_form.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2024, chandru and Contributors
 # See license.txt
 
-# import frappe
-from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.frappe.tests.utils import FrappeTestCase
 
 
 class TestCustomizeForm(FrappeTestCase):
