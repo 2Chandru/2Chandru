b8d8e9e03e6c57b5a5b03484699cca5a80ce91e6
2Chandru <chandru231100@gmail.com>
2024-02-15 10:03:37 +0530

    Added untracked files in my_app/doctype/

---
+++ my_app/my_app/doctype/customize_form/test_customize_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, chandru and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomizeForm(FrappeTestCase):
+	pass
