652646de5b9e4533e158574aa44a708db0f53816
2Chandru <chandru231100@gmail.com>
2024-02-15 11:47:51 +0530

    Update customize_form.py and test_customize_form.py for XYZ feature

--- my_app/my_app/doctype/customize_form/customize_form.py
+++ my_app/my_app/doctype/customize_form/customize_form.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, chandru and contributors
 # For license information, please see license.txt
 import frappe
-from frappe.frappe.model.document import Document
+from frappe.model.document import Document
 
 
 class CustomizeForm(Document):
--- my_app/my_app/doctype/customize_form/test_customize_form.py
+++ my_app/my_app/doctype/customize_form/test_customize_form.py
@@ -2,7 +2,7 @@
 # See license.txt
 
 import frappe
-from frappe.frappe.tests.utils import FrappeTestCase
+from frappe.tests.utils import FrappeTestCase
 
 
 class TestCustomizeForm(FrappeTestCase):
